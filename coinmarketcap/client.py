9a01041d752af6fa06f2766d3e9da3fd5dd5efa0
Andreas Nilsen <andnil94@gmail.com>
2019-03-04 19:48:32 +0100

    added throttling of requests

--- coinmarketcap/client.py
+++ coinmarketcap/client.py
@@ -7,11 +7,19 @@ from requests.exceptions import HTTPError
 
 # local
 from .endpoints import Cryptocurrency, Exchange, GlobalMetrics, Tools
-from .environment import Sandbox, Production
+from .environment import Sandbox, Production, Throttler
 
 
 class Client(Sandbox, Production):
-    def __init__(self, apikey=None, expire=3600, sandbox=False):
+    def __init__(
+        self,
+        apikey=None,
+        plan="hobbyist",
+        expire=3600,
+        sandbox=False,
+        throttle="monthly",
+        block=True,
+    ):
         if sandbox:
             Sandbox.__init__(self, apikey, expire)
         else:
@@ -21,6 +29,7 @@ class Client(Sandbox, Production):
         self.global_metrics = GlobalMetrics(self.request)
         self.exchange = Exchange(self.request)
         self.tools = Tools(self.request)
+        self.throttler = Throttler(plan, throttle, block)
 
     def request(self, urn, params):
         url = Request("GET", urljoin(self.url, urn), params=params).prepare().url
@@ -35,12 +44,24 @@ class Client(Sandbox, Production):
             res["cached"] = response.from_cache
             return res
         else:
-            raise HTTPError(
-                "%d - %s" % (response.status_code, res["status"]["error_message"])
-            )
+            try:
+                raise HTTPError(
+                    "%d - %s" % (response.status_code, res["status"]["error_message"])
+                )
+            except KeyError:
+                raise HTTPError(response.status_code)
 
     def _request(self, url):
         return self.session.get(url)
 
     def _request_throttle(self, url):
+        self.throttler.throttle()
         return self.session.get(url)
+
+    @property
+    def plan(self):
+        return self.throttler.plan
+
+    @plan.setter
+    def plan(self, minute=0, daily=0, monthly=0):
+        self.throttler.plan(minute, daily, monthly)
--- coinmarketcap/environment.py
+++ coinmarketcap/environment.py
@@ -4,8 +4,12 @@ from os.path import join
 from pathlib import Path
 from tempfile import gettempdir
 from requests_cache.core import CachedSession as session
+from threading import Lock
 from json import load
 from os import environ
+from ratelimit import limits, sleep_and_retry
+from datetime import datetime
+from calendar import monthrange
 
 FILE = ".coinmarketcap.json"
 
@@ -56,3 +60,80 @@ class Production(Session):
                     raise KeyError("Can not locate key.")
 
         Session.__init__(self, apikey, expire, cf)
+
+
+class Plan:
+    __plans = {
+        "free": (10, 333, 10000),
+        "hobbyist": (60, 1333, 40000),
+        "startup": (60, 4000, 120000),
+        "standard": (60, 16666, 500000),
+        "professional": (60, 100000, 3000000),
+        "enterprise": (0, 0, 0),
+    }
+
+    def __init__(self, plan):
+        if plan not in self.__plans:
+            raise ValueError
+        self._plan = self.__plans[plan]
+
+    @property
+    def plan(self):
+        return self._plan
+
+    @plan.setter
+    def plan(self, minute=0, daily=0, monthly=0):
+        self._plan = (minute, daily, monthly)
+
+    @property
+    def minute(self):
+        period = 60
+        calls = self.plan[0]
+        return calls, period
+
+    @property
+    def daily(self):
+        period = (86400 / self.plan[1]) * self.plan[0]
+        calls = self.plan[0]
+        return calls, period
+
+    @property
+    def monthly(self):
+        now = datetime.now()
+        days = monthrange(now.year, now.month)[1]
+
+        period = ((days * 86400) / self.plan[2]) * self.plan[0]
+        calls = self.plan[0]
+        return calls, period
+
+
+class Throttler(Plan):
+    def __init__(self, plan, throttle, block):
+        Plan.__init__(self, plan)
+        self.throttling = True
+        self.lock = Lock
+        scheme = (0, 0)
+
+        if throttle is None:
+            self.throttling = False
+        elif throttle == "minute":
+            scheme = self.minute
+        elif throttle == "daily":
+            scheme = self.daily
+        elif throttle == "monthly":
+            scheme = self.monthly
+        else:
+            raise ValueError("Argument throttle must be either ")
+        print(scheme)
+
+        self.limit = limits(*scheme, raise_on_limit=block)(lambda x: None)
+        self.sleep = sleep_and_retry(self.limit)
+        self.block = block
+
+    def throttle(self):
+        if self.throttling:
+            with self.lock:
+                if self.block:
+                    self.sleep(credit)
+                else:
+                    self.limit(credit)
